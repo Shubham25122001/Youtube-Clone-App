da69e1d7ab0ed779eb9153f7228222cf3f0b8740
shubham verma <101853403+Shubham25122001@users.noreply.github.com>
2023-11-27 10:10:09 +0530

    api key change

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Spinner() {
   return (
     <div className="flex items-center w-full justify-center py-3">
-      <div className="w-10 h-10 border-2 border-red-600 border-solid rounded-full animate-spin border-t-transparent"></div>
+      <div className="w-10 h-10 border-2 border-blue-600 border-solid rounded-full animate-spin border-t-transparent"></div>
     </div>
   );
 }
